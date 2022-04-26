467524a8d688d00c6ecc1e0ecb52a59564ce3adc
Ian Chen <ichen2@lsu.edu>
2022-04-26 17:43:36 -0500

    Added downloader script

---
+++ downloader.py
@@ -0,0 +1,16 @@
+import requests, zipfile, glob, os
+
+BASE_URL = "https://cims.coastal.louisiana.gov/RequestedDownloads/ZippedFiles/"
+data_types = ["Full_Continuous_Hydrographic", "Full_Discrete_Hydrographic", "Full_Marsh_Vegetation", "Full_Forest_Vegetation", "Full_Surface_Elevation", "Full_Soil_Properties", "Full_Accretion", "Full_Biomass"]
+for data_type in data_types:
+  url = BASE_URL + data_type + ".zip"
+  print("Downloading file from " + url)
+  response = requests.get(url)
+  with open(data_type + ".zip", "wb") as output_file:
+    output_file.write(response.content)
+  with zipfile.ZipFile(data_type + ".zip","r") as zip_ref:
+    zip_ref.extractall("./data")
+  # delete temporary files created during download and unzipping
+  for file in glob.glob(data_type + "*"):
+    os.remove(file)
+  print("Download complete!")
--- scraper.py
+++ scraper.py
@@ -6,10 +6,9 @@ from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
 
-data_types = ["soil_properties"]
+data_types = ["hydro_monthly", "accretion", "forestveg", "marshveg", "biomass", "soil_properties", "surface_elevation"]
 for data_type in data_types:
-    driver.get(
-        "https://cims.coastal.louisiana.gov/DataDownload/DataDownload.aspx?type=" + data_type)
+    driver.get("https://cims.coastal.louisiana.gov/DataDownload/DataDownload.aspx?type=" + data_type)
     project_element = driver.find_element(By.ID, "MainContent_DDL_ProjectList")
     options = project_element.find_elements(By.TAG_NAME, "option")
     option_values = []
@@ -25,7 +24,6 @@ for data_type in data_types:
         project_select.select_by_value(option_value)
         driver.find_element(By.ID, "MainContent_BTN_DownLoad").click()
         input_box = driver.find_element(By.ID, "MainContent_TB_Filename")
-        # Gives file name 'hello'
         input_box.send_keys(data_type + "_" + str(index + 1))
         driver.find_element(By.ID, "MainContent_BTN_OkFilename").click()
         WebDriverWait(driver, timeout=3)
