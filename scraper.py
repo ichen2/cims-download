c3b8668995f8c0b3319142ca59a7cec44495b974
Ian Chen <ichen2@lsu.edu>
2022-04-26 13:54:50 -0500

    Made script cycle through all project options.

--- scraper.py
+++ scraper.py
@@ -10,10 +10,16 @@ driver.get(
 project_element = driver.find_element(By.ID, "MainContent_DDL_ProjectList")
 project_select = Select(project_element)
 options = project_element.find_elements(By.TAG_NAME, "option")
+option_values = []
+for option in options:
+    option_values.append(option.get_attribute("value"))
 station_element = driver.find_element(By.ID, "MainContent_DDL_Stations")
 station_select = Select(station_element)
-# for option in options:
-#     project_select.select_by_value(option.value)
-#     WebDriverWait(driver, timeout=3)
+for option_value in option_values:
+    # need to get the project selector again since the page reloads
+    project_select = Select(driver.find_element(
+        By.ID, "MainContent_DDL_ProjectList"))
+    project_select.select_by_value(option_value)
+    WebDriverWait(driver, timeout=3)
 
 driver.quit()
