1fd658510644107637b01ccd3343d6a64298d674
Ian Chen <ichen2@lsu.edu>
2022-04-26 16:30:42 -0500

    Added file naming. Added an array of data_types that the script can iterate through.

--- scraper.py
+++ scraper.py
@@ -6,26 +6,28 @@ from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
 
-driver.get(
-    "https://cims.coastal.louisiana.gov/DataDownload/DataDownload.aspx?type=soil_properties")
-project_element = driver.find_element(By.ID, "MainContent_DDL_ProjectList")
-options = project_element.find_elements(By.TAG_NAME, "option")
-option_values = []
-for option in options:
-    option_values.append(option.get_attribute("value"))
+data_types = ["soil_properties"]
+for data_type in data_types:
+    driver.get(
+        "https://cims.coastal.louisiana.gov/DataDownload/DataDownload.aspx?type=" + data_type)
+    project_element = driver.find_element(By.ID, "MainContent_DDL_ProjectList")
+    options = project_element.find_elements(By.TAG_NAME, "option")
+    option_values = []
+    for option in options:
+        option_values.append(option.get_attribute("value"))
 
-station_element = driver.find_element(By.ID, "MainContent_DDL_Stations")
-station_select = Select(station_element)
+    station_element = driver.find_element(By.ID, "MainContent_DDL_Stations")
+    station_select = Select(station_element)
 
-for option_value in option_values[1:]:
-    project_select = Select(driver.find_element(
-        By.ID, "MainContent_DDL_ProjectList"))
-    project_select.select_by_value(option_value)
-    driver.find_element(By.ID, "MainContent_BTN_DownLoad").click()
-    input_box = driver.find_element(By.ID, "MainContent_TB_Filename")
-    #Gives file name 'hello'
-    input_box.send_keys("hello")
-    driver.find_element(By.ID, "MainContent_BTN_OkFilename").click()
-    WebDriverWait(driver, timeout=3)
+    for index, option_value in enumerate(option_values[1:]):
+        project_select = Select(driver.find_element(
+            By.ID, "MainContent_DDL_ProjectList"))
+        project_select.select_by_value(option_value)
+        driver.find_element(By.ID, "MainContent_BTN_DownLoad").click()
+        input_box = driver.find_element(By.ID, "MainContent_TB_Filename")
+        # Gives file name 'hello'
+        input_box.send_keys(data_type + "_" + str(index + 1))
+        driver.find_element(By.ID, "MainContent_BTN_OkFilename").click()
+        WebDriverWait(driver, timeout=3)
 
 driver.quit()
